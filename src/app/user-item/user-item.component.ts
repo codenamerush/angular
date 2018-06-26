9a1a1e484c0eb8b686fb33b7aa75ea06fa3364c6
Aarushi <aarushisharda@gmail.com>
2018-06-26 22:36:09 +0530

    Used the @Input decorator

--- src/app/user-item/user-item.component.ts
+++ src/app/user-item/user-item.component.ts
@@ -1,17 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-
-@Component({
+import {
+  Component,
+  OnInit,
+  Input // <--- added this
+  } from '@angular/core';
+  @Component({
   selector: 'app-user-item',
   templateUrl: './user-item.component.html',
   styleUrls: ['./user-item.component.css']
-})
-export class UserItemComponent implements OnInit {
-  name: string;
+  })
+  export class UserItemComponent implements OnInit {
+  @Input() name: string; // <-- added Input annotation
   constructor() {
-    this.name = "Rush";
-   }
-
-  ngOnInit() {
+  // removed setting name
   }
-
-}
+  ngOnInit() {}
+  }
